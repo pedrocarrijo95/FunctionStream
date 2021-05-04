1c4edbaeb7ce96bbae76dcc58c7d58a6bb59deb7
pedrocarrijo <pedro.carrijo@oracle.com>
2021-05-04 10:05:36 -0300

    a

--- func.js
+++ func.js
@@ -45,10 +45,10 @@ console.log("mensagem:"+ jsonstring);
   const streamId = stream.id;
 
   // publish some messages to the stream
-  await publishExampleMessages(client, streamId);
+  //await publishExampleMessages(client, streamId);
 
   // give the streaming service a second to propagate messages
-  await delay(1);
+  //await delay(1);
 
   // Use a cursor for getting messages; each getMessages call will return a next-cursor for iteration.
   // There are a couple kinds of cursors.
@@ -123,13 +123,13 @@ async function getOrCreateStream(compartmentId, paritions, exampleStreamName) {
   return activeStream.stream;
 }
 
-async function publishExampleMessages(client, streamId) {
+/**async function publishExampleMessages(client, streamId) {
   // build up a putRequest and publish some messages to the stream
   let messages = [];
   for (let i = 1; i <= 3; i++) {
     let entry = {
-      key: Buffer.from("ChaveMensagemViaFunc" + i).toString("base64"),
-      value: Buffer.from("ValorMensagemViaFunc" + i).toString("base64")
+      key: Buffer.from("mkey: " + i).toString("base64"),
+      value: Buffer.from("mvalue: " + i).toString("base64")
     };
     messages.push(entry);
   }
@@ -143,7 +143,7 @@ async function publishExampleMessages(client, streamId) {
   const putMessageResponse = await client.putMessages(putMessagesRequest);
   for (var entry of putMessageResponse.putMessagesResult.entries)
     console.log("Published messages to parition %s, offset %s", entry.partition, entry.offset);
-}
+}**/
 
 async function getCursorByPartition(client, streamId, partition) {
   console.log("Creating a cursor for partition %s.", partition);
