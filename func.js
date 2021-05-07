2f9d644a726f6d9e073ab4d4f03b95bf1bfc7a0e
pedrocarrijo <pedro.carrijo@oracle.com>
2021-05-07 07:21:32 -0300

    teste

--- func.js
+++ func.js
@@ -1,6 +1,5 @@
 const fdk=require('@fnproject/fdk');
 
-
 var fs = require('fs');
 const st = require("oci-streaming");
 const common = require("oci-common");
@@ -10,9 +9,12 @@ var messageslist = [];
 //oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_19_8' });
 
 fdk.handle(function(data){
-
-  console.log(data);
-  return {'message': 'Sucesso: '+data}
+  let dados = "nao chegou nada";
+  if(data){
+    dados = data;
+  }
+  console.log(dados);
+  return {'message': 'Sucesso: '+dados}
 
 })
 
