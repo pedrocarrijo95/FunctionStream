a256eaf656260ca68ffdc1b5c3deb2ba9696cedc
pedrocarrijo <pedro.carrijo@oracle.com>
2021-05-07 13:01:37 -0300

    testando axios

--- func.js
+++ func.js
@@ -1,11 +1,11 @@
 const fdk=require('@fnproject/fdk');
 var request = require('request');
+const axios = require('axios');
 
-
-fdk.handle(function(data){
+fdk.handle(async function(input){
   
 
-    try{
+    /**try{
       //console.log(data);
       //if(data[0].key != null){
         //var messagekey = Buffer.from(data[0].key.toString(), "base64").toString();
@@ -19,14 +19,37 @@ fdk.handle(function(data){
     }
     catch(err){
       console.log("erro: "+err.message);
-    }
+    }**/
+
+    var data = JSON.stringify({"name":"msgFUNC","count":500});
 
 
-    return {'message': 'Sucesso'}
+    var config = {
+      method: 'post',
+      url: 'https://k3knrkyh5wbw6k8-ajdatabase.adb.us-ashburn-1.oraclecloudapps.com/ords/admin/soda/latest/fruit',
+      headers: { 
+        'Authorization': 'Basic YWRtaW46T3JhY2xlMTIzNDU2', 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    var resp = "a";
+    (async () => {
+      try {
+        const response = await axios(config);
+        resp = response.data;
+        console.log(response.data);
+      } catch (error) {
+        console.log(error.response.body);
+      }
+    })();
+
+
+    return {'message': 'Sucesso: '+response}
 
 })
 
-//sodaInsert("fruit","a","b");
+//sodaInsert("fruit","teste","testes");
 
 function sodaInsert(collection,messagekey,messagevalue){
   var options = {
@@ -52,4 +75,8 @@ function sodaInsert(collection,messagekey,messagevalue){
 async function delay(s) {
   return new Promise(resolve => setTimeout(resolve, s * 1000));
 }
+
+
+
+
 
